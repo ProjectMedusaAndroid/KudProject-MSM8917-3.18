a71461cfbb1cb35af5104eef1955b1cda4e3aa9d
imoseyon <imoseyon@gmail.com>
2017-07-13 20:51:35 +0100

    cpufreq: interactive: add screen off max frequency tunable
    @nathanchance: updated to current interactive from this commit: https://github.com/imoseyon/leanKernel-angler/commit/465224c1ed4d9686aaabdb7e176ea70a65ae1295
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -164,6 +164,10 @@ struct cpufreq_interactive_tunables {
 	/* Improves frequency selection for more energy */
 	bool powersave_bias;
 
+	/* Maximum frequency while the screen is off */
+#define DEFAULT_SCREEN_OFF_MAX 1248000
+	unsigned long screen_off_max;
+
 #if defined(CONFIG_ARCH_MSM8953) || defined(CONFIG_ARCH_MSM8917)
 
 	unsigned int lpm_disable_freq;
@@ -766,6 +770,11 @@ static int cpufreq_interactive_speedchange_task(void *data)
 			if (!ppol->governor_enabled) {
 				up_read(&ppol->enable_sem);
 				continue;
+			}
+
+			if (unlikely(!display_on)) {
+			    if (ppol->target_freq > tunables->screen_off_max)
+				ppol->target_freq = tunables->screen_off_max;
 			}
 
 			if (ppol->target_freq != ppol->policy->cur) {
@@ -1477,6 +1486,32 @@ static ssize_t store_powersave_bias(struct cpufreq_interactive_tunables *tunable
 	return count;
 }
 
+static ssize_t show_screen_off_maxfreq(
+		struct cpufreq_interactive_tunables *tunables,
+                char *buf)
+{
+	return sprintf(buf, "%lu\n", tunables->screen_off_max);
+}
+
+static ssize_t store_screen_off_maxfreq(
+		struct cpufreq_interactive_tunables *tunables,
+                const char *buf, size_t count)
+{
+	int ret;
+	unsigned long val;
+
+	ret = kstrtoul(buf, 0, &val);
+	if (ret < 0)
+		return ret;
+
+	if (val < 384000)
+		tunables->screen_off_max = DEFAULT_SCREEN_OFF_MAX;
+	else
+		tunables->screen_off_max = val;
+
+	return count;
+}
+
 /*
  * Create show/store routines
  * - sys: One governor instance for complete SYSTEM
@@ -1532,6 +1567,8 @@ show_store_gov_pol_sys(ignore_hispeed_on_notif);
 show_store_gov_pol_sys(fast_ramp_down);
 show_store_gov_pol_sys(enable_prediction);
 show_store_gov_pol_sys(powersave_bias);
+show_store_gov_pol_sys(screen_off_maxfreq);
+
 #if defined(CONFIG_ARCH_MSM8953) || defined(CONFIG_ARCH_MSM8917)
 show_store_gov_pol_sys(lpm_disable_freq);
 #endif
@@ -1566,6 +1603,8 @@ gov_sys_pol_attr_rw(ignore_hispeed_on_notif);
 gov_sys_pol_attr_rw(fast_ramp_down);
 gov_sys_pol_attr_rw(enable_prediction);
 gov_sys_pol_attr_rw(powersave_bias);
+gov_sys_pol_attr_rw(screen_off_maxfreq);
+
 #if defined(CONFIG_ARCH_MSM8953) || defined(CONFIG_ARCH_MSM8917)
 gov_sys_pol_attr_rw(lpm_disable_freq);
 #endif
@@ -1597,6 +1636,8 @@ static struct attribute *interactive_attributes_gov_sys[] = {
 	&fast_ramp_down_gov_sys.attr,
 	&enable_prediction_gov_sys.attr,
 	&powersave_bias_gov_sys.attr,
+	&screen_off_maxfreq_gov_sys.attr,
+
 #if defined(CONFIG_ARCH_MSM8953) || defined(CONFIG_ARCH_MSM8917)
 	&lpm_disable_freq_gov_sys.attr,
 #endif
@@ -1629,6 +1670,7 @@ static struct attribute *interactive_attributes_gov_pol[] = {
 	&fast_ramp_down_gov_pol.attr,
 	&enable_prediction_gov_pol.attr,
 	&powersave_bias_gov_pol.attr,
+	&screen_off_maxfreq_gov_pol.attr,
 #if defined(CONFIG_ARCH_MSM8953) || defined(CONFIG_ARCH_MSM8917)
 	&lpm_disable_freq_gov_pol.attr,
 #endif
@@ -1671,6 +1713,7 @@ static struct cpufreq_interactive_tunables *alloc_tunable(
 	tunables->timer_rate = DEFAULT_TIMER_RATE;
 	tunables->boostpulse_duration_val = DEFAULT_MIN_SAMPLE_TIME;
 	tunables->timer_slack_val = DEFAULT_TIMER_SLACK;
+	tunables->screen_off_max = DEFAULT_SCREEN_OFF_MAX;
 
 	spin_lock_init(&tunables->target_loads_lock);
 	spin_lock_init(&tunables->above_hispeed_delay_lock);
