983e4afd0f0a439c04cfeb4f4401722047494e0a
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2020-07-18 23:12:24 +0300

    kernel/power/powersuspend: new PM kernel driver for Android  w/o early suspend v1.5 (faux123/Yank555.lu)

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -39,6 +39,10 @@ extern unsigned int is_boot_recovery;
 #endif
 #include "mdss_livedisplay.h"
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -994,6 +998,11 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1262,7 +1271,17 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	}
 #endif
 
-	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
+	pr_info("%s: mdss_dsi_panel_off time=%ums \n", __func__,
+		jiffies_to_msecs(jiffies-timeout));
+
+	if (ctrl->ds_registered && pinfo->is_pluggable) {
+		mdss_dba_utils_video_off(pinfo->dba_data);
+		mdss_dba_utils_hdcp_enable(pinfo->dba_data, false);
+	}
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 end:
 	/* clear idle state */
